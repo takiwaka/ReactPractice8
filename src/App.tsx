9ff9c4c6f9892725d4444afccc91a4b756f133c9
kazuya.kawakita <61093699+takiwaka@users.noreply.github.com>
2021-12-12 10:36:10 +0900

    axios指定

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import axios from "axios";
+import { useState } from "react";
 import { UserCard } from "./components/UserCard";
 import "./styles.css";
+import { User } from "./types/api/user";
+import { USerProfile } from "./types/UserProfile";
 
 const user = {
   id: 1,
@@ -8,9 +12,21 @@ const user = {
   address: "東京"
 };
 
-const onClickGetData = () => {};
-
 export default function App() {
+  const [UserProfiles, setUserProfiles] = useState<Array<USerProfile>>([]);
+  const onClickGetData = () => {
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+      });
+  };
+
   return (
     <div className="App">
       <button onClick={onClickGetData}>データ取得</button>
